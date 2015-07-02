e9f50245e719f370c8935abe0e6274d82bc537fb
sangamarket <sanga.market@gmail.com>
2015-07-02 22:44:18 +0530

    Update Login.java

--- src/com/thecherno/chernochat/Login.java
+++ src/com/thecherno/chernochat/Login.java
@@ -95,7 +95,7 @@ public class Login extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("sanga2");
+		System.out.println("sanga2asdfafasfa");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
