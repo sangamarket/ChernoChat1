14f3cd03587e924a1c818e93d557ddebe88e752e
sangamarket <sanga.market@gmail.com>
2015-07-02 22:54:13 +0530

    sa

--- src/com/thecherno/chernochat/Login.java
+++ src/com/thecherno/chernochat/Login.java
@@ -95,7 +95,7 @@ public class Login extends JFrame {
 	}
 
 	public static void main(String[] args) {
-
+		System.out.println("some");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
